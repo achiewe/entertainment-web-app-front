8cd0d94405cbecdcd65a818972ab2eb23a2de3cf
achiewe <mamukashviliachi706@gmail.com>
2023-09-12 13:40:54 +0400

    create new folder for component signup and login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import styled from "styled-components";
 import GlobalStyles from "./GlobalStyles";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <GlobalStyles />
-    </div>
+    <Router>
+      <MainContainer>
+        <GlobalStyles />
+      </MainContainer>
+    </Router>
   );
 }
+
+const MainContainer = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #10141e;
+`;
 
 export default App;
