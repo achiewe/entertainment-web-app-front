68c467f8e9f2e82448b8a22cf4b4ce6711ab625c
achiewe <mamukashviliachi706@gmail.com>
2023-09-12 17:10:29 +0400

    create userValidation.tsx and create userSchema to validate email and password

--- src/components/UserAccess/UserValidation.ts
+++ src/components/UserAccess/UserValidation.ts
@@ -1,1 +1,6 @@
 import * as yup from "yup";
+
+export const userSchema = yup.object().shape({
+  email: yup.string().email().required("Can't be empty"),
+  password: yup.string().min(4).max(10).required("Can't be empty"),
+});
