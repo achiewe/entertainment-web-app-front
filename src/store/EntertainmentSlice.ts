ad542b281d261337f5bcf3d281f17c0076b7acc7
achiewe <mamukashviliachi706@gmail.com>
2023-09-13 21:17:04 +0400

    finish the enitertainmentSlice component

--- src/store/EntertainmentSlice.ts
+++ src/store/EntertainmentSlice.ts
@@ -1,9 +1,23 @@
-import entertainmentType from "../../type";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import entertainment from "../../type";
 
 export interface entertaiProps {
-  entertainment: entertainmentType[];
+  entertainment: entertainment[];
 }
 
 const initialState: entertaiProps = {
   entertainment: [],
 };
+
+const entertainmentSlice = createSlice({
+  name: "entertainment",
+  initialState,
+  reducers: {
+    setEntertainment: (state, action: PayloadAction<entertainment[]>) => {
+      state.entertainment = action.payload;
+    },
+  },
+});
+
+export const { setEntertainment } = entertainmentSlice.actions;
+export default entertainmentSlice.reducer;
