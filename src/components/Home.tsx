66f53cc630b72830e3cfab6dfab3a41dc72fc9fd
achiewe <mamukashviliachi706@gmail.com>
2023-09-15 15:52:57 +0400

    create Home.tsx component and styled it

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,14 @@
+import styled from "styled-components";
+
 const Home = (): JSX.Element => {
-  return <div> </div>;
+  return <MainHome> </MainHome>;
 };
 
+const MainHome = styled.div`
+  width: 100%;
+  min-width: 100vh;
+  display: flex;
+  flex-direction: column;
+  gap: 26px;
+`;
 export default Home;
