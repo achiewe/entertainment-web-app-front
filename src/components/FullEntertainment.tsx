4a44fc4ca72fe4ecca2e59108e4e9c7edbd37075
achiewe <mamukashviliachi706@gmail.com>
2023-09-16 12:58:57 +0400

    create new component FullEntertainment where will be everything info in data.json

---
+++ src/components/FullEntertainment.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const FullEntertainment = (): JSX.Element => {
+  return <EntertainmentCont> </EntertainmentCont>;
+};
+
+const EntertainmentCont = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 16px;
+  gap: 16px;
+`;
+
+export default FullEntertainment;
