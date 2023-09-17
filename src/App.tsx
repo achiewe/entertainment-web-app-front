a28c41082c9ea933716a48190a3c680339918a8c
achiewe <mamukashviliachi706@gmail.com>
2023-09-17 17:22:13 +0400

    add in main.tsx provider for wrap react-application

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,29 @@ import Login from "./components/UserAccess/Login";
 import SignUp from "./components/UserAccess/SignUp";
 import Home from "./components/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import axios from "axios";
+import entertainmentType from "../type";
+import { useDispatch } from "react-redux";
+import { setEntertainment } from "./store/EntertainmentSlice";
+import { useEffect } from "react";
 
 function App() {
+  const dis = useDispatch();
+
+  const takeInfo = async (): Promise<void> => {
+    const url = "http://localhost:3000/takeEntertainment";
+    try {
+      const response = await axios.get<entertainmentType[]>(url);
+      dis(setEntertainment(response.data));
+    } catch (error) {
+      console.log("can't take data");
+    }
+  };
+
+  useEffect(() => {
+    takeInfo();
+  }, []);
+
   return (
     <Router>
       <MainContainer>
