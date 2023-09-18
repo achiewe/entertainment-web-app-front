9b25d4c13efdaafae9ad768e11d3cb35de373658
achiewe <mamukashviliachi706@gmail.com>
2023-09-18 17:16:36 +0400

    create new redux component for user log in

---
+++ src/store/LoggedInSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export interface logProps {
+  logIn: boolean;
+}
+
+const initialState: logProps = {
+  logIn: false,
+};
+
+const LoggedInSlice = createSlice({
+  name: "login",
+  initialState,
+  reducers: {
+    setLogIn: (state, action: PayloadAction<boolean>) => {
+      state.logIn = action.payload;
+    },
+  },
+});
+
+export const { setLogIn } = LoggedInSlice.actions;
+export default LoggedInSlice;
