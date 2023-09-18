7f047ae8228a20321bb20122996078f1e9c69d7a
achiewe <mamukashviliachi706@gmail.com>
2023-09-18 13:22:49 +0400

    add clientEmailSlice in redux.ts for use it in code

--- src/store/redux.ts
+++ src/store/redux.ts
@@ -1,14 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import EntertainmentSlice, { entertaiProps } from "./EntertainmentSlice";
+import clientEmailSlice, { userProps } from "./clientEmailSlice";
 
 const store = configureStore({
   reducer: {
     entertainment: EntertainmentSlice,
+    clientEmail: clientEmailSlice,
   },
 });
 
 export type RootState = {
   entertainment: entertaiProps;
+  clientEmail: userProps;
 };
 
 export default store;
