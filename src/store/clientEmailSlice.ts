24d2bd9c0404413132bb46554d070f543fc72ffe
achiewe <mamukashviliachi706@gmail.com>
2023-09-18 13:21:28 +0400

    create redux file and new function setClientEmail for save client email info here

--- src/store/clientEmailSlice.ts
+++ src/store/clientEmailSlice.ts
@@ -1,3 +1,5 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
 export interface userProps {
   clientEmail: string;
 }
@@ -5,3 +7,16 @@ export interface userProps {
 const initialState: userProps = {
   clientEmail: "",
 };
+
+const clientEmailSlice = createSlice({
+  name: "clientEmail",
+  initialState,
+  reducers: {
+    setClientEmail: (state, action: PayloadAction<string>) => {
+      state.clientEmail = action.payload;
+    },
+  },
+});
+
+export const { setClientEmail } = clientEmailSlice.actions;
+export default clientEmailSlice.reducer;
