7b8dad0071bb99c94341163fc6b464310540ca41
achiewe <mamukashviliachi706@gmail.com>
2023-09-30 00:57:21 +0400

    add svg with ternary operator logic

--- src/components/TrendingEnt.tsx
+++ src/components/TrendingEnt.tsx
@@ -1,4 +1,9 @@
 import styled from "styled-components";
+import fullBookTrend from "../../public/assets/icon-bookmark-full.svg";
+import emptyBookTrend from "../../public/assets/icon-bookmark-empty.svg";
+import dotSvg from "../../public/assets/Oval.svg";
+import serieSvgTrend from "../../public/assets/icon-nav-tv-series.svg";
+import movieSvgTrend from "../../public/assets/icon-nav-movies.svg";
 import { motion } from "framer-motion";
 import { useEffect, useRef } from "react";
 import { useState } from "react";
@@ -42,14 +47,37 @@ const TrendingEnt = (): JSX.Element => {
             className="innerCarousel"
           >
             {isTrendFilter.map((trend) => (
-              <motion.div className="item">
+              <motion.div className="item" key={trend._id}>
                 <img
                   className="ImgTrend"
                   src={trend.thumbnail.trending.small}
                 />
 
                 <div className="trendingStructure">
-                  <div className="bookmarkTrend"></div>
+                  <div className="bookmarkTrend">
+                    <img
+                      src={trend.isBookmarked ? fullBookTrend : emptyBookTrend}
+                      alt="bookmark svg"
+                    />
+                  </div>
+                  <div className="TrendTitleDiv">
+                    <div className="infoTrand">
+                      <h4> year</h4>
+                      <img src={dotSvg} className="dot" />
+                      <img
+                        src={
+                          trend.category === "Movie"
+                            ? movieSvgTrend
+                            : serieSvgTrend
+                        }
+                        className="movieSerielog"
+                      />
+                      <h4> {trend.category}</h4>
+                      <img src={dotSvg} className="dot" />
+                      <h4> {trend.rating}</h4>
+                    </div>
+                    <h2> {trend.title}</h2>
+                  </div>
                 </div>
               </motion.div>
             ))}
@@ -92,6 +120,7 @@ const TrendingMain = styled.div`
       width: 240px;
       height: 140px;
       border-radius: 8px;
+      position: relative;
 
       .ImgTrend {
         width: 100%;
@@ -100,12 +129,17 @@ const TrendingMain = styled.div`
       }
 
       .trendingStructure {
+        position: absolute;
         width: 100%;
         display: flex;
         flex-direction: column;
         padding: 8px 8px 16px 16px;
         align-items: flex-end;
         gap: 46px;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
 
         .bookmarkTrend {
           width: 32px;
@@ -116,6 +150,49 @@ const TrendingMain = styled.div`
           align-items: center;
           background: rgba(6, 0, 27, 0.501);
           z-index: 2;
+        }
+
+        .TrendTitleDiv {
+          display: flex;
+          flex-direction: column;
+          width: 100%;
+          justify-content: flex-start;
+          align-items: flex-start;
+          .infoTrand {
+            width: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: flex-start;
+            flex-direction: row;
+            gap: 6px;
+
+            h4 {
+              color: #fff;
+              font-size: 12px;
+              font-style: normal;
+              font-weight: 300;
+              line-height: normal;
+              opacity: 0.75;
+            }
+
+            .dot {
+              width: 3px;
+              height: 3px;
+            }
+
+            .movieSerielog {
+              width: 12px;
+              height: 12px;
+            }
+          }
+
+          h2 {
+            color: #fff;
+            font-size: 15px;
+            font-style: normal;
+            font-weight: 500;
+            line-height: normal;
+          }
         }
       }
     }
