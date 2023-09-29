a109f11d3e6dfcf807247a4f69ac13e6189ca16a
achiewe <mamukashviliachi706@gmail.com>
2023-09-29 23:24:45 +0400

    add trending section some slice for it

--- src/components/TrendingEnt.tsx
+++ src/components/TrendingEnt.tsx
@@ -1,25 +1,55 @@
 import styled from "styled-components";
+import { motion } from "framer-motion";
+import { useEffect, useRef } from "react";
+import { useState } from "react";
 
 const TrendingEnt = (): JSX.Element => {
+  const [width, setWidth] = useState<number | undefined>(0);
+  const carousel = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const current = carousel.current;
+    if (current) {
+      if (
+        typeof current.scrollWidth !== "undefined" &&
+        typeof current.offsetWidth !== "undefined"
+      ) {
+        setWidth(current.scrollWidth - current.offsetWidth);
+      }
+    }
+  }, []);
+
   return (
     <TrendingMain>
       <h2> Trending</h2>
-      <div className="silderDiv">
-        <div className="trendingInfoDiv"></div>
-        <div className="trendingInfoDiv"></div>
-        <div className="trendingInfoDiv"></div>
-        <div className="trendingInfoDiv"></div>
-        <div className="trendingInfoDiv"></div>
-      </div>
+      <motion.div
+        ref={carousel}
+        className="carousel"
+        whileDrag={{ cursor: "grabbing" }}
+      >
+        <motion.div
+          drag="x"
+          dragConstraints={{ right: 2000, left: -width! }} // Use the non-null assertion operator (!) here
+          className="innerCarousel"
+        >
+          <motion.div className="item">1</motion.div>
+          <motion.div className="item">2</motion.div>
+          <motion.div className="item">3</motion.div>
+          <motion.div className="item">4</motion.div>
+          <motion.div className="item">5</motion.div>
+        </motion.div>
+      </motion.div>
     </TrendingMain>
   );
 };
 
 const TrendingMain = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: column;
   gap: 16px;
   align-items: flex-start;
+  overflow: hidden;
 
   h2 {
     color: #fff;
@@ -30,15 +60,24 @@ const TrendingMain = styled.div`
     letter-spacing: -0.312px;
   }
 
-  .silderDiv {
-    display: flex;
-    overflow-x: scroll;
-  }
+  .carousel {
+    cursor: grab;
+    overflow-x: hidden;
+    background-color: white;
 
-  .trendingInfoDiv {
-    width: 10px;
-    height: 10px;
-    background-color: red;
+    .innerCarousel {
+      display: flex;
+      background-color: #10141e;
+      gap: 12px;
+    }
+
+    .item {
+      min-width: 240px;
+      min-height: 70px;
+      background-color: red;
+      padding: 40px;
+      border-radius: 8px;
+    }
   }
 `;
 export default TrendingEnt;
