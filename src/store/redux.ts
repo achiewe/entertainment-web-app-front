c11920a98f82ca59b1b2a7f41a1e867846af4b72
achiewe <mamukashviliachi706@gmail.com>
2023-10-02 16:08:28 +0400

    add value for use it in components

--- src/store/redux.ts
+++ src/store/redux.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import EntertainmentSlice, { entertaiProps } from "./EntertainmentSlice";
 import clientEmailSlice, { userProps } from "./ClientEmailSlice";
 import LoggedInSlice, { logProps } from "./LoggedInSlice";
+import InputFilterSlice, { inputProps } from "./InputFilterSlice";
 
 const store = configureStore({
   reducer: {
     entertainment: EntertainmentSlice,
     clientEmail: clientEmailSlice,
     logIn: LoggedInSlice,
+    value: InputFilterSlice,
   },
 });
 
@@ -15,6 +17,7 @@ export type RootState = {
   entertainment: entertaiProps;
   clientEmail: userProps;
   logIn: logProps;
+  value: inputProps;
 };
 
 export default store;
